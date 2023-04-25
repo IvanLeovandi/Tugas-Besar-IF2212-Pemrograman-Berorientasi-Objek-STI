8cf0788906c655fb338a4978f98d9081f6b00d03
IvanLeovandi <16521021@std.stei.itb.ac.id>
2023-04-25 13:03:31 +0700

    update furniture

--- Game/src/com/simplicity/Furniture/Clock.java
+++ Game/src/com/simplicity/Furniture/Clock.java
@@ -3,7 +3,7 @@ import com.simplicity.*;
 
 public class Clock extends Furniture{
     public Clock(){
-        super("Jam", new Point(1,1), 10, 8);
+        super("Jam", new Dimension2D(1,1), 10, 8);
     }
 
     // method melihat waktu
