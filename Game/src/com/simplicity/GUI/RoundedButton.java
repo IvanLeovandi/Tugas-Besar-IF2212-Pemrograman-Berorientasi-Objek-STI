04f64834bc31bdfd08f2d7bc924c7c02fbeac9fc
IvanLeovandi <16521021@std.stei.itb.ac.id>
2023-04-26 22:53:02 +0700

    adding a bit gui, will be updated asap hiks

---
+++ Game/src/com/simplicity/GUI/RoundedButton.java
@@ -0,0 +1,36 @@
+package com.simplicity.GUI;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JButton;
+
+public class RoundedButton extends JButton {
+    private static final long serialVersionUID = 1L;
+    private static final int ARC_WIDTH = 10;
+    private static final int ARC_HEIGHT = 10;
+
+    public RoundedButton(String text) {
+        super(text);
+        setOpaque(false);
+        setContentAreaFilled(false);
+        setBorderPainted(false);
+        setPreferredSize(new Dimension(100, 30));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if (getModel().isPressed()) {
+            g2.setColor(getBackground().darker());
+        } 
+        else {
+            g2.setColor(getBackground());
+        }
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), ARC_WIDTH, ARC_HEIGHT);
+        g2.dispose();
+        super.paintComponent(g);
+    }
+}
