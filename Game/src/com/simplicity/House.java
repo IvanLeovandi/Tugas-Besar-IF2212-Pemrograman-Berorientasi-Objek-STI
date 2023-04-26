d461b2b5e47a60fb363b97b44bcb2a369ee0db39
Asweh Debrej <aswehdebrej@gmail.com>
2023-04-27 00:43:41 +0700

    mengbuat

--- Game/src/com/simplicity/House.java
+++ Game/src/com/simplicity/House.java
@@ -1,5 +1,57 @@
 package com.simplicity;
 
-public class House {
+import java.util.*;
 
+import com.simplicity.Exceptions.OverlapingWorldObjectException;
+import com.simplicity.Exceptions.PlacementOutOfBoundException;
+import com.simplicity.Interfaces.WorldObject;
+
+public class House implements WorldObject {
+    protected String type = "rumah";
+    private Dimension2D size;
+    private Map<Point, WorldObject> map = new HashMap<>();
+
+    public House(int width, int length) {
+        size = new Dimension2D(width, length);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < length; j++) {
+                map.put(new Point(i, j), null);
+            }
+        }
+    }
+
+    public void addContent(Point p, WorldObject wo) throws PlacementOutOfBoundException, OverlapingWorldObjectException {
+        if (p.getX() < 0 || p.getY() < 0 || wo.getSize().getLength() + p.getX() > size.getLength() || wo.getSize().getWidth() + p.getY() > size.getWidth()) {
+            throw new PlacementOutOfBoundException(wo);
+        }
+
+        int iMax = size.getWidth();
+        int jMax = size.getLength();
+        for (int i = 0; i < iMax; i++) {
+            for (int j = 0; j < jMax; j++) {
+                WorldObject objectCheck = map.get(new Point(i, j));
+                if (objectCheck != null) {
+                    throw new OverlapingWorldObjectException(wo, objectCheck);
+                }
+            }
+        }
+
+        for (int i = p.getX(); i < p.getX() + wo.getSize().getLength(); i++) {
+            for (int j = p.getY(); j < p.getY() + wo.getSize().getWidth(); j++) {
+                map.replace(new Point(i, j), wo);
+            }
+        }
+    }
+
+    public void removeContent(Point p) {}
+
+    public void removeContent(WorldObject wo) {}
+
+    public String getType() {
+        return type;
+    }
+
+    public Dimension2D getSize() {
+        return size;
+    }
 }
--- Game/src/com/simplicity/Job.java
+++ Game/src/com/simplicity/Job.java
@@ -2,18 +2,33 @@ package com.simplicity;
 
 public class Job {
     private int salaryPerDay;
+    private String name;
 
-    private Job(int salaryPerDay) {
+    private Job(String name, int salaryPerDay) {
         this.salaryPerDay = salaryPerDay;
+        this.name = name;
     }
 
-    public static final Job clown   = new Job(15);
-    public static final Job cook    = new Job(30);
-    public static final Job police  = new Job(35);
-    public static final Job programmer  = new Job(45);
-    public static final Job doctor  = new Job(50);
+    public static final Job clown       = new Job("badut", 15);
+    public static final Job CLOWN       = clown;
+    public static final Job cook        = new Job("koki", 30);
+    public static final Job COOK        = cook;
+    public static final Job police      = new Job("polisi", 35);
+    public static final Job POLICE      = police;
+    public static final Job programmer  = new Job("programmer", 45);
+    public static final Job PROGRAMMER  = programmer;
+    public static final Job doctor      = new Job("dokter", 50);
+    public static final Job DOCTOR      = doctor;
 
     public int getSalaryPerDay() {
         return salaryPerDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "Job: " + name;
     }
 }
