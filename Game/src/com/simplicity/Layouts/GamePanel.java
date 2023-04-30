2d4c92aa79bac4f007c65488ae6bf87c82f2ac51
IvanLeovandi <16521021@std.stei.itb.ac.id>
2023-04-30 23:46:46 +0700

    update game button

--- Game/src/com/simplicity/Layouts/GamePanel.java
+++ Game/src/com/simplicity/Layouts/GamePanel.java
@@ -30,32 +30,81 @@ public class GamePanel extends JPanel implements HousePickListener {
         this.add(sideInfo, BorderLayout.EAST);
     }
 
+    private class MenuButton extends JButton {
+        public MenuButton(String text) {
+            this.setPreferredSize(new Dimension(200, 40));
+            this.setText(text);
+            
+        }
+    }
+
     private class SideMenu extends JPanel {
+        GridBagConstraints gbc;
+        int compNum = 0;
+
         public SideMenu() {
             this.setPreferredSize(new Dimension(280, 720));
             this.setLayout(new GridBagLayout());
             this.setBackground(new Color(0x9961f2));
-            GridBagConstraints gbc = new GridBagConstraints();
+            gbc = new GridBagConstraints();
             gbc.insets = new Insets(10, 10, 10, 10);
             gbc.anchor = GridBagConstraints.NORTHWEST;
             gbc.fill = GridBagConstraints.NONE;
-            gbc.gridx = 0;
-            gbc.gridy = 0;
+            gbc.gridx = compNum;
+            gbc.gridy = compNum;
 
-            for (int i = 0; i < 5; i++) {
-                JButton button = new JButton("aan " + (i+1));
-                button.setPreferredSize(new Dimension(200, 40));
-                gbc.gridy = i % 10;
-                gbc.gridx = i / 10;
-                this.add(button, gbc);
-            }
+            MenuButton actionButton = new MenuButton("ACTION");
+            actionButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("");
+                }
+            });
+            this.addComponent(actionButton);
 
-            this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+            MenuButton simInfoButton = new MenuButton("VIEW SIM INFO");
+            this.addComponent(simInfoButton);
+
+            MenuButton currentLocationButton = new MenuButton("VIEW CURRENT LOCATION");
+            this.addComponent(currentLocationButton);
+
+            MenuButton inventoryButton = new MenuButton("VIEW INVENTORY");
+            this.addComponent(inventoryButton);
+
+            MenuButton upgradeHouseButton = new MenuButton("UPGRADE HOUSE");
+            this.addComponent(upgradeHouseButton);
+
+            MenuButton moveRoomButton = new MenuButton("MOVE ROOM");
+            this.addComponent(moveRoomButton);
+
+            MenuButton editRoomButton = new MenuButton("EDIT ROOM");
+            this.addComponent(editRoomButton);
+
+            MenuButton addSimButton = new MenuButton("ADD SIM");
+            this.addComponent(addSimButton);
+
+            MenuButton changeSimButton = new MenuButton("CHANGE SIM");
+            this.addComponent(changeSimButton);
+
+            MenuButton listObjectButton = new MenuButton("LIST OBJECT");
+            this.addComponent(listObjectButton);
+
+            MenuButton goToObjectButton = new MenuButton("GO TO OBJECT");
+            this.addComponent(goToObjectButton);
+            
+        }
+
+        public void addComponent(JComponent c) {
+            gbc.gridy = compNum % 11;
+            gbc.gridx = compNum / 11;
+            this.add(c, gbc);
+            compNum++;
         }
     }
 
     private class SideInfo extends JPanel {
         JLabel context = new JLabel();
+
         public SideInfo() {
             context.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
             context.setAlignmentY(TOP_ALIGNMENT);
@@ -87,7 +136,7 @@ public class GamePanel extends JPanel implements HousePickListener {
     @Override
     public void onHousePick(HousePickEvent e) {
         Point location = e.getPoint();
-        housePanel = (HousePanel)world.getHouse(location).getPanel();
+        housePanel = (HousePanel) world.getHouse(location).getPanel();
         setCurrentCenterPanel(housePanel);
     }
 }
---
+++ Game/src/com/simplicity/Layouts/SimCreatePanel.java
@@ -0,0 +1,6 @@
+package com.simplicity.Layouts;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
